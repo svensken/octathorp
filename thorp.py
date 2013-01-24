6960bf67bb81d9bba064bf283545987d8a866422
ken <svensken@nbcs.rutgers.edu>
2013-01-24 17:08:49 -0500

    the loops are finally sane. much improvement needed, still

--- thorp.py
+++ thorp.py
@@ -44,17 +44,29 @@ residue2 = 33
 
 
 
-#####
-# select the atoms based on input
+######
+## select the atoms based on input
+## Stub 1
+#s1a1 = AtomID(1, residue1 - 1)
+#s1a2 = AtomID(1, residue1)
+#s1a3 = AtomID(1, residue1 + 1)
+#stub1 = StubID(s1a1, s1a2, s1a3)
+## Stub 2
+#s2a1 = AtomID(1, residue2 - 1)
+#s2a2 = AtomID(1, residue2)
+#s2a3 = AtomID(1, residue2 + 1)
+#stub2 = StubID(s2a1, s2a2, s2a3)
+
+# define stubs as single-residue
 # Stub 1
-s1a1 = AtomID(1, residue1 - 1)
-s1a2 = AtomID(1, residue1)
-s1a3 = AtomID(1, residue1 + 1)
+s1a1 = AtomID(1, residue1)
+s1a2 = AtomID(2, residue1)
+s1a3 = AtomID(3, residue1)
 stub1 = StubID(s1a1, s1a2, s1a3)
 # Stub 2
-s2a1 = AtomID(1, residue2 - 1)
-s2a2 = AtomID(1, residue2)
-s2a3 = AtomID(1, residue2 + 1)
+s2a1 = AtomID(1, residue2)
+s2a2 = AtomID(2, residue2)
+s2a3 = AtomID(3, residue2)
 stub2 = StubID(s2a1, s2a2, s2a3)
 
 print stub1
@@ -68,10 +80,11 @@ print stub2
 # 4: RT core.kinematics.AtomTree.get_stub_transform(stub1,stub2)
 
 pose_atom_tree = pose.atom_tree()
-jump = pose_atom_tree.get_stub_transform(stub1, stub2)
+reference_jump = pose_atom_tree.get_stub_transform(stub1, stub2)
 
-ref_transl_list = [float(n) for n in str(jump.get_translation()).split()]
-ref_rotatn_list = [float(n) for n in str(jump.get_rotation()).split()]
+# break the numbers down
+reference_transl_list = [float(n) for n in str(reference_jump.get_translation()).split()]
+reference_rotatn_list = [float(n) for n in str(reference_jump.get_rotation()).split()]
 
 
 ####################################################
@@ -111,46 +124,55 @@ for residue in range(1,new_pose.total_residue()+1):
     # first stub consists of atomno's 1, 2, and 3 of res#1
     # last stub consists of atomno's 1, 2, and 3 of res#last
     # Stub 1
-    n_s1a1 = AtomID(1, residue - 1)
-    n_s1a2 = AtomID(1, residue)
-    n_s1a3 = AtomID(1, residue + 1)
+    n_s1a1 = AtomID(1, residue)
+    n_s1a2 = AtomID(2, residue)
+    n_s1a3 = AtomID(3, residue)
     n_stub = StubID(n_s1a1, n_s1a2, n_s1a3)
     print n_stub
     stub_list.append(n_stub)
 
-
 raw_input('hit enter to continue')
 #print stub_list[57].atom(2) # middle
 
-
+# build jump list
 new_pose_atom_tree = new_pose.atom_tree()
 jump_list = []
+n = 0
 for first_stub in stub_list:
-    for second_stub in stub_list:
-        new_jump = new_pose_atom_tree.get_stub_transform(first_stub, second_stub)
-        print new_jump
-        jump_list.append(new_jump)
+    for second_stub in stub_list[n:]: # start at subsequent stub (to first stub)
+        new_jump_obj = new_pose_atom_tree.get_stub_transform(first_stub, second_stub)
+        jump_list_element = [first_stub, second_stub, new_jump_obj]
+        # structure of jump_list elements:
+        # [ ---   int   int   obj    --- ]
+        # [ ..., [res1, res2, jump], ... ]
+        jump_list.append(jump_list_element)
+    n = n + 1
 
+for jamp in jump_list:
+    temp_transl_list = [float(n) for n in str(jamp[2].get_translation()).split()]
+    temp_rotatn_list = [float(n) for n in str(jamp[2].get_rotation()).split()]
+    
+    # check comparison against tolerance
+    try:
+        transl_diff = [a/b for a,b in zip(reference_transl_list, temp_transl_list)]
+    except ZeroDivisionError:
+        pass
+    try:
+        rotatn_diff = [a/b for a,b in zip(reference_rotatn_list, temp_rotatn_list)]
+    except ZeroDivisionError:
+        pass
+    try:
+        comprehensive_diff = transl_diff + rotatn_diff
+        for diff in comprehensive_diff:
+            if abs(diff - 1) >= .1 :
+                print index, " is no match"
+                # is one bad value enough to ignore whole jump?
+                break
+            else:
+                print "found a close one; ", jamp[0], ' to ', jamp[1]
+    except:
+        print "figure out how to handle zeroes"
 
-#temp_transl_list = [float(n) for n in str(jump.get_translation()).split()]
-#temp_transl_list = [float(n) for n in str(jump.get_translation()).split()]
-
-# check match against tolerance
-#transl_diff = [a/b for a,b in zip(ref_transl_list, transl_list2)]
-#rotatn_diff = [a/b for a,b in zip(ref_rotatn_list, rotatn_list2)]
-
-#comprehensive_diff = transl_diff + rotatn_diff
-#if [diff for diff in comprehensive_diff 
-#for diff in comprehensive_diff:
-#    if abs(diff - 1) >= .1 :
-#        print "no match"
-#        break
-#    else:
-#        print "good"
-#        print stub1
-#        print stub2
-
-# would be faster with izip and immediate checks to break
 
 ########
 # deem loop's secondary composition
