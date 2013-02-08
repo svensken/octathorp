6a398f4607756feea950ed5dfc39e636049dc1e7
ken <svensken@nbcs.rutgers.edu>
2013-02-08 00:11:16 -0500

    write vs read still taking shape. suddenly dreadfully slow

---
+++ read-thorp.py
@@ -0,0 +1,73 @@
+from rosetta import *
+
+init()
+
+
+
+##################################################
+#- Get reference jump----------------------------#
+
+## load reference pose
+pose = Pose()
+try:
+    pose_from_pdb( pose, 'alpha-beta-hydrolases/' + pdb_file )
+    pose_load_result = 'no error'
+except PyRosettaException:
+    pose_load_result = 'slight error'
+
+output_string = '\n\n##################################################\n'+ \
+    'This file contains output from the thorp.py script\n'+ \
+    'invoked at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n'+ \
+    'Reference PDB: '+str(pdb_file)+'\n'+ \
+    'Reference jump residues: '+str(residue1)+' and '+str(residue2)+'\n'+ \
+    str(pose_load_result)+' loading the reference pose (no need to worry)\n'+ \
+    'total reference residues: '+str(pose.total_residue())+'\n\n'
+f.write(output_string)
+f.flush()
+
+## calculate reference jump
+# Stub 1
+s1a1 = AtomID(1, residue1 - 1)
+s1a2 = AtomID(1, residue1)
+s1a3 = AtomID(1, residue1 + 1)
+stub1 = StubID(s1a1, s1a2, s1a3)
+# Stub 2
+s2a1 = AtomID(1, residue2 - 1)
+s2a2 = AtomID(1, residue2)
+s2a3 = AtomID(1, residue2 + 1)
+stub2 = StubID(s2a1, s2a2, s2a3)
+
+#------------------------------------------------#
+##################################################
+
+
+
+# reconstruct an RT object from a stored string
+m = numeric.xyzMatrixdouble(1)
+m = m.rows(11,12,13,14,15,16,17,18,19)
+v = numeric.xyzVector_double(1,2,3)
+
+rt = RT()
+rt.set_rotation(m)
+rt.set_translation(v)
+
+#print rt
+#print rt.get_translation()
+#print rt.get_rotation()
+
+all_s = ''
+for life in os.listdir('alpha-beta-hydrolases/')[:10]:
+    try:
+        pose=Pose()
+        pose_from_pdb(pose, 'alpha-beta-hydrolases/'+life)
+    except:
+        pass
+    
+    DsspMover().apply(pose)
+    all_s +=  pose.secstruct()
+
+
+
+
+# compare
+rmsd = distance( reference_jump, this_jump )
--- write-thorp.py
+++ write-thorp.py
@@ -9,55 +9,12 @@ init()
 # (pass flag to ignore unrecognized residues)
 
 
-def find_similar_transforms( pdb_file, residue1, residue2 ):
+def get_all_transforms( ):
 
     raw_input("please run 'tail -f loggy' to see all script output, then press enter")
     
-    #TODO check chain of res
-    #TODO disect workings of distance()
-    #TODO pickle the overall jump_list to begin with
     #TODO detect domains (for res select without input)
     ## --> ExPASy's Prosite, Conserved Domain db, ESTHER db, ...
-
-    ##################################################
-    #- Get reference jump----------------------------#
-
-    ## load reference pose
-    pose = Pose()
-    try:
-        pose_from_pdb( pose, 'alpha-beta-hydrolases/' + pdb_file )
-        pose_load_result = 'no error'
-    except PyRosettaException:
-        pose_load_result = 'slight error'
-
-    output_string = '\n\n##################################################\n'+ \
-        'This file contains output from the thorp.py script\n'+ \
-        'invoked at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n'+ \
-        'Reference PDB: '+str(pdb_file)+'\n'+ \
-        'Reference jump residues: '+str(residue1)+' and '+str(residue2)+'\n'+ \
-        str(pose_load_result)+' loading the reference pose (no need to worry)\n'+ \
-        'total reference residues: '+str(pose.total_residue())+'\n\n'
-    f.write(output_string)
-    f.flush()
-
-    ## calculate reference jump
-    # Stub 1
-    s1a1 = AtomID(1, residue1 - 1)
-    s1a2 = AtomID(1, residue1)
-    s1a3 = AtomID(1, residue1 + 1)
-    stub1 = StubID(s1a1, s1a2, s1a3)
-    # Stub 2
-    s2a1 = AtomID(1, residue2 - 1)
-    s2a2 = AtomID(1, residue2)
-    s2a3 = AtomID(1, residue2 + 1)
-    stub2 = StubID(s2a1, s2a2, s2a3)
-
-    pose_atom_tree = pose.atom_tree()
-    reference_jump = pose_atom_tree.get_stub_transform(stub1, stub2)
-
-    #------------------------------------------------#
-    ##################################################
-
 
 
     ##################################################
@@ -65,9 +22,9 @@ def find_similar_transforms( pdb_file, residue1, residue2 ):
 
     # load all other PDB's
     # cleaned pdb's end with 'A'
-    clean_pdb_file_list = [item for item in os.listdir('alpha-beta-hydrolases/') if item[-5:] == 'A.pdb']
+    pdb_file_list = [item for item in os.listdir('alpha-beta-hydrolases/') if item[-5:] == 'A.pdb']
 
-    output_string = '\n'+str(len(clean_pdb_file_list))+' PDBs to posify and check jumps from:\n'
+    output_string = '\n'+str(len(pdb_file_list))+' PDBs to posify and check jumps from:\n'
     f.write(output_string)
     f.flush()
 
@@ -76,74 +33,71 @@ def find_similar_transforms( pdb_file, residue1, residue2 ):
     n=0
     t0 = time.time()
 #TODO remove list limit
-    for clean_pdb_file_name in clean_pdb_file_list[:3]: # first 100 files only
+    for pdb_file_name in pdb_file_list[:3]: # first 100 files only
         n=n+1
         t1 = time.time()
         try:
-            new_pose = Pose()
-            pose_from_pdb( new_pose, 'alpha-beta-hydrolases/' + clean_pdb_file_name )
+            pose = Pose()
+            pose_from_pdb( pose, 'alpha-beta-hydrolases/' + pdb_file_name )
             pose_load_result = '.'
         except PyRosettaException:
             pose_load_result = '*'
 
         stub_list = []
-        for middle_residue in range(2,new_pose.total_residue()):
+        for middle_residue in range(2, pose.total_residue()):
             # first stub's central residue is #2
             # last stub's central residue is #last-1
             # Stub 1
-            n_s1a1 = AtomID(1, middle_residue - 1)
-            n_s1a2 = AtomID(1, middle_residue)
-            n_s1a3 = AtomID(1, middle_residue + 1)
-            n_stub = StubID(n_s1a1, n_s1a2, n_s1a3)
-            stub_list.append(n_stub)
+            s1a1 = AtomID(1, middle_residue - 1)
+            s1a2 = AtomID(1, middle_residue)
+            s1a3 = AtomID(1, middle_residue + 1)
+            stub = StubID(s1a1, s1a2, s1a3)
+            stub_list.append(stub)
 
         # populate pose's secstruct
-        DsspMover().apply(new_pose) # populate secstruct
-        full_ss = new_pose.secstruct()
+        DsspMover().apply(pose) # populate secstruct
+        full_ss = pose.secstruct()
         # infotize
-        pdb_info = new_pose.pdb_info()
+        pdb_info = pose.pdb_info()
+        # atom-tree-ify
+        pose_atom_tree = pose.atom_tree()
 
-        new_pose_atom_tree = new_pose.atom_tree()
         i = 1
         for first_stub in stub_list:
             for second_stub in stub_list[ i: ]: # start at subsequent stub (to first_stub)
 
-                new_jump = new_pose_atom_tree.get_stub_transform(first_stub, second_stub)
+                jump = pose_atom_tree.get_stub_transform(first_stub, second_stub)
 
                 chunk_ss = full_ss[ first_stub.atom(2).rsd() : second_stub.atom(2).rsd() ]
 
-                
                 # define things to store
-                this_jump    = str(new_jump)
-                this_pdb_id  = clean_pdb_file_name
-                # pdb_info already defined
+                this_jump    = str(jump)
+                this_pdb_id  = pdb_file_name
                 this_1st_res = pdb_info.pose2pdb( first_stub.atom(2).rsd() )
                 this_2nd_res = pdb_info.pose2pdb( second_stub.atom(2).rsd() )
-                this_rmsd    = distance( reference_jump, new_jump )
                 this_ss_L    = chunk_ss.count('L') / len(chunk_ss)
                 this_ss_E    = chunk_ss.count('E') / len(chunk_ss)
                 this_ss_H    = chunk_ss.count('H') / len(chunk_ss)
 
-
                 # define tolerances
-                q_rmsd = this_rmsd < 5
-                q_len  = new_pose.total_residue() < 500 # just to speed up first run
+                q_len  = pose.total_residue() < 500 # just to speed up first run
                 # minimum E and H in ss
                 
-                if q_rmsd and q_len:
+                if True and q_len:
                     # structure of a good_jump:
-                    # [ pdb, res1, res2, diff ]
-                    # name could also be clean_pdb_file_name
-                    good_jump = [          \
-                        this_jump,         \
-                        this_pdb_id,       \
-                        this_1st_res,      \
-                        this_2nd_res,      \
-                        this_rmsd ]
+                    # [ jump, pdb-id, res1, res2, ss_L, ss_E, ss_H ]
+                    good_jump = [           \
+                        this_jump,          \
+                        this_pdb_id,        \
+                        this_1st_res,       \
+                        this_2nd_res,       \
+                        this_ss_L,          \
+                        this_ss_E,          \
+                        this_ss_H  ]
                     best_jumps.append(good_jump)
             i = i + 1
 
-        if n%50==0 or n==len(clean_pdb_file_list):
+        if n%50==0 or n==len(pdb_file_list):
             f.write('\n###'+str(n)+'###\n')
         else:
             f.write(str(round(time.time()-t1,3))+'s, ')
@@ -154,11 +108,13 @@ def find_similar_transforms( pdb_file, residue1, residue2 ):
 
     best_10_jumps_string = sorted( best_jumps, key=operator.itemgetter(3) )[ :10 ]
 
+    return best_10_jumps_string
+
     output_string = \
         '\n\nAlles Klar\n\
          Time taken to load and grade all possible jumps: '+str(time.time()-t0)+' seconds\n\n'+ \
         'Best 10 jumps: \n'+ \
-        '\n'.join( [str(L) for L in best_10_jumps_string] )
+        '' #'\n'.join( [str(L) for L in best_10_jumps_string] )
     f.write(output_string)
     f.flush()
 
@@ -199,7 +155,7 @@ if __name__ == '__main__':
         #job_server = pp.Server()
         #for cup in job_server.get_ncpus():
 
-        find_similar_transforms( pdb_file, residue1, residue2 )
+        best_jumps = get_all_transforms( )
             
 # close log file
 f.close()
