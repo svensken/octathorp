1e4677c89c23173ad0399759d74b86ee7dadef0f
Kenny <svensken@nbcs.rutgers.edu>
2013-03-06 16:58:02 -0500

    thorp.py glitching, need to figure out pose passing issue

--- thorp.py
+++ thorp.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 from rosetta import *
+from structural_alignment import kabsch_alignment
 import os, sys, time, datetime
+#import pymol
 
 init()
 
 
-def find_matching_domains( family_dir, pdb_file, residue1, residue2, out_file=matchy, debug_file=loggy ):
+def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, loggy ):
     
     ## calculate reference jump
     # load reference pose
@@ -73,19 +75,21 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, out_file=ma
                 rt = pose_atom_tree.get_stub_transform(first_stub, second_stub)
                 rmsd = distance( ref_rt, rt )
 
-                if rmsd < 1:
+                if rmsd < 10:
                     # parameters to both check against and record to file
                     chunk_ss = full_ss[ first_stub.atom(2).rsd() : second_stub.atom(2).rsd() ]
                     this_ss_L    = chunk_ss.count('L') / float( len(chunk_ss) )
                     this_ss_E    = chunk_ss.count('E') / float( len(chunk_ss) )
                     this_ss_H    = chunk_ss.count('H') / float( len(chunk_ss) )
 
-                    jump_length  = jump.get_translation().length
+                    jump_length  = rt.get_translation().length
 
                     # TODO need better way to access residue numbers
-                    this_1st_res = pdb_info.pose2pdb( first_stub.atom(2).rsd() )
-                    this_2nd_res = pdb_info.pose2pdb( second_stub.atom(2).rsd() )
-                    loop_length  = int(this_2nd_res.split()[0]) - int(this_1st_res.split()[0])
+                    first_res_string  = pdb_info.pose2pdb( first_stub.atom(2).rsd() )
+                    second_res_string = pdb_info.pose2pdb( second_stub.atom(2).rsd() )
+                    first_res  = int(first_res_string.split()[0])
+                    second_res = int(second_res_string.split()[0])
+                    loop_length  = second_res - first_res
 
                     # tolerances for good matches
                     #TODO define at top of script, exec string (easily editable, and we can print tolerances into all_transforms file later)
@@ -93,16 +97,16 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, out_file=ma
                     q_jump_length   = jump_length               < 8
                     q_loop_length   = loop_length               < 80
 
-                    if q_jump_length and q_loop_length:
+                    if pdb_file_name[-5:] != pdb_file[-5:] and True: #q_jump_length and q_loop_length:
 
-                        good_match = [          \
-                            pdb_file_name,      \
-                            this_1st_res,       \
-                            this_2nd_res,       \
-                            jump_length,        \
-                            loop_length,        \
-                            this_ss_L,          \
-                            this_ss_E,          \
+                        good_match = [    
+                            pdb_file_name,
+                            first_res, 
+                            second_res, 
+                            jump_length,  
+                            loop_length,  
+                            this_ss_L,    
+                            this_ss_E,    
                             this_ss_H  ]
                         # OUPUT
                         for item in good_match:
@@ -110,13 +114,15 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, out_file=ma
                         matchy.write( '\n' )
                         matchy.flush()
                         
+                        print pdb_file_name
+                        print pdb_file
                         # TODO make this part optional
-                        construct_pose_from_matching_domains( pose,                # pose
+                        construct_pose_from_matching_domains( pose,                 # pose
                                                               residue1,             # int
                                                               residue2,             # int
-                                                              row_list[1],          # '1aaaA.pdb'
-                                                              int(row_items[2]),    # int
-                                                              int(row_items[3])  )  # int
+                                                              pdb_file_name,         # '1aaaA.pdb'
+                                                              first_res,            # int
+                                                              second_res   )        # int
 
             i = i + 1
 
@@ -124,9 +130,9 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, out_file=ma
     loggy.write( '\nrun took ' + str(time.time()-t0) + ' seconds total\n')
     loggy.flush()
 
-        
-    
-    
+
+
+
 def construct_pose_from_matching_domains( host_pose,    # pose
                                           host_res1,    # int
                                           host_res2,    # int
@@ -137,21 +143,25 @@ def construct_pose_from_matching_domains( host_pose,    # pose
     guest_pose = Pose()
     pose_from_pdb( guest_pose, 'alpha-beta-hydrolases/'+guest_name )
 
+    print host_pose
+    print guest_pose
     # rotate guest pose to align with host's ref residues
-    kabsch_alignment( host_pose, guest_pose, [ host_res1 - 1 ,      \
-                                               host_res1     ,      \
-                                               host_res1 + 1 ,      \
-                                               host_res2 - 1 ,      \
-                                               host_res2     ,      \
-                                               host_res2 + 1   ],   \
-                                             [ guest_res1 - 1 ,     \
-                                               guest_res1     ,     \
-                                               guest_res1 + 1 ,     \
-                                               guest_res2 - 1 ,     \
-                                               guest_res2     ,     \
-                                               guest_res2 + 1  ] )  \
+    kabsch_alignment( host_pose, guest_pose, [ host_res1 - 1 ,
+                                               host_res1     ,
+                                               host_res1 + 1 ,
+                                               host_res2 - 1 ,
+                                               host_res2     ,
+                                               host_res2 + 1   ],
+                                             [ guest_res1 - 1 ,
+                                               guest_res1     ,
+                                               guest_res1 + 1 ,
+                                               guest_res2 - 1 ,
+                                               guest_res2     ,
+                                               guest_res2 + 1  ] )
 
     pymover = PyMOL_Mover() 
+    print host_pose
+    print guest_pose
     pymover.apply(host_pose)
     pymover.apply(guest_pose)
     raw_input('see pymol for match')
@@ -159,14 +169,14 @@ def construct_pose_from_matching_domains( host_pose,    # pose
     # generate new pose from aligned domains
     new_pose = Pose()
 
-    f.write( str(host_pose)  )
-    f.write( str(guest_pose) )
-    f.flush()
+    loggy.write( str(host_pose)  )
+    loggy.write( str(guest_pose) )
+    loggy.flush()
 
 
 
 
-f.close()
+
 
 if __name__ == '__main__':
 
@@ -188,10 +198,10 @@ if __name__ == '__main__':
         # DEBUG OUTPUT
         output_string = '\n\n#######################################\n'+ \
             'This file contains debugging output for thorp.py\n'+ \
-            'invoked at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n' \
+            'invoked at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n'+ \
             'Reference PDB: '+str(pdb_file)+'\n'+ \
             'Residues '+str(residue1)+' and '+str(residue2)+'\n'+ \
-            str(len(os.listdir(family_dir))+' PDBs to posify and traverse for transforms\n\n'
+            str(len(os.listdir(family_dir)))+' PDBs to posify and traverse for transforms\n\n'
         loggy.write(output_string)
         loggy.flush()
 
@@ -199,11 +209,12 @@ if __name__ == '__main__':
         # TODO output tolerances
         matchy_string = \
             'This list from '+family_dir+' directory, ### pdbs, *** tolerances, etc \n' + \
-            'saved at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n'
+            'saved at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n'+ \
+            str(sys.argv)+'\n\n\n'
         matchy.write(matchy_string)
         matchy.flush()
         
-        find_matching_domains( family_dir, pdb_file, residue1, residue2 ) # out_ and debug_files need not be passed
+        find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, loggy )
 
         #view_matching_domains()
         ## cycles through by default
