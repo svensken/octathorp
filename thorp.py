900cb1e83f2609f4207c43f7bd314ee026dd0e9b
Kenny <svensken@nbcs.rutgers.edu>
2013-03-08 14:55:13 -0500

    some nasty workarounds with pymol interface, ugly mac pymol

--- thorp.py
+++ thorp.py
@@ -3,18 +3,24 @@
 from rosetta import *
 from structural_alignment import kabsch_alignment
 import os, sys, time, datetime
-#import pymol
+
+# hell of an annoyance, this pymol messiness
+sys.path.append('/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages/')
+import pymol
+
+pymol.finish_launching()
+pymol.cmd.do('run ~/Desktop/PyRosetta/PyMOLPyRosettaServer.py')
 
 init()
 
 
-def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, loggy ):
+def find_matching_domains( family_dir, orig_pdb_file, residue1, residue2, matchy, loggy ):
     
     ## calculate reference jump
     # load reference pose
-    pose = Pose()
+    orig_pose = Pose()
     try:
-        pose_from_pdb( pose, pdb_file )
+        pose_from_pdb( orig_pose, orig_pdb_file )
         pose_load_result = 'no error'
     except PyRosettaException:
         pose_load_result = 'slight error'
@@ -29,8 +35,8 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
     s2a3 = AtomID(1, residue2 + 1)
     stub2 = StubID(s2a1, s2a2, s2a3)
     # Transform
-    pose_atom_tree = pose.atom_tree()
-    ref_rt = pose_atom_tree.get_stub_transform( stub1, stub2 )
+    orig_pose_atom_tree = orig_pose.atom_tree()
+    orig_rt = orig_pose_atom_tree.get_stub_transform( stub1, stub2 )
 
 
     # build pdb file list
@@ -38,12 +44,12 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
 
     n=0
     t0 = time.time()
-    for pdb_file_name in pdb_file_list:
+    for cur_pdb_file_name in pdb_file_list:
         n=n+1
         t1 = time.time()
         try:
-            pose = Pose()
-            pose_from_pdb( pose, family_dir + pdb_file_name )
+            cur_pose = Pose()
+            pose_from_pdb( cur_pose, family_dir + cur_pdb_file_name )
             pose_load_result = '.'
         except PyRosettaException:
             pose_load_result = '*'
@@ -51,7 +57,7 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
             continue
 
         stub_list = []
-        for middle_residue in range(2, pose.total_residue()):
+        for middle_residue in range(2, cur_pose.total_residue()):
             # first stub's central residue is #2
             # last stub's central residue is #last-1
             s1a1 = AtomID(1, middle_residue - 1)
@@ -61,19 +67,19 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
             stub_list.append(stub)
 
         # populate pose's secstruct
-        DsspMover().apply(pose) # populate secstruct
-        full_ss = pose.secstruct()
+        DsspMover().apply(cur_pose) # populate secstruct
+        full_ss = cur_pose.secstruct()
         # infotize
-        pdb_info = pose.pdb_info()
+        pdb_info = cur_pose.pdb_info()
         # atom-tree-ify
-        pose_atom_tree = pose.atom_tree()
+        cur_pose_atom_tree = cur_pose.atom_tree()
 
         i = 1
         for first_stub in stub_list:
             for second_stub in stub_list[ i: ]: # start at subsequent stub (to first_stub)
 
-                rt = pose_atom_tree.get_stub_transform(first_stub, second_stub)
-                rmsd = distance( ref_rt, rt )
+                cur_rt = cur_pose_atom_tree.get_stub_transform(first_stub, second_stub)
+                rmsd = distance( orig_rt, cur_rt )
 
                 if rmsd < 10:
                     # parameters to both check against and record to file
@@ -82,29 +88,29 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
                     this_ss_E    = chunk_ss.count('E') / float( len(chunk_ss) )
                     this_ss_H    = chunk_ss.count('H') / float( len(chunk_ss) )
 
-                    jump_length  = rt.get_translation().length
+                    jump_length  = cur_rt.get_translation().length
 
                     # TODO need better way to access residue numbers
                     first_res_string  = pdb_info.pose2pdb( first_stub.atom(2).rsd() )
                     second_res_string = pdb_info.pose2pdb( second_stub.atom(2).rsd() )
-                    first_res  = int(first_res_string.split()[0])
-                    second_res = int(second_res_string.split()[0])
-                    loop_length  = second_res - first_res
+                    cur_first_res  = int(first_res_string.split()[0])
+                    cur_second_res = int(second_res_string.split()[0])
+                    cur_loop_length  = cur_second_res - cur_first_res
 
                     # tolerances for good matches
                     #TODO define at top of script, exec string (easily editable, and we can print tolerances into all_transforms file later)
-                    q_tot_res       = pose.total_residue()      < 500 
-                    q_jump_length   = jump_length               < 8
-                    q_loop_length   = loop_length               < 80
+                    q_tot_res       = cur_pose.total_residue()      < 500 
+                    q_jump_length   = jump_length                   < 8
+                    q_loop_length   = cur_loop_length               < 80
 
-                    if pdb_file_name[-5:] != pdb_file[-5:] and True: #q_jump_length and q_loop_length:
+                    if cur_pdb_file_name != orig_pdb_id and True: #q_jump_length and q_loop_length:
 
                         good_match = [    
-                            pdb_file_name,
-                            first_res, 
-                            second_res, 
+                            cur_pdb_file_name,
+                            cur_first_res, 
+                            cur_second_res, 
+                            cur_loop_length,  
                             jump_length,  
-                            loop_length,  
                             this_ss_L,    
                             this_ss_E,    
                             this_ss_H  ]
@@ -114,15 +120,15 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
                         matchy.write( '\n' )
                         matchy.flush()
                         
-                        print pdb_file_name
-                        print pdb_file
+                        print cur_pdb_file_name
+                        print orig_pdb_file
                         # TODO make this part optional
-                        construct_pose_from_matching_domains( pose,                 # pose
+                        construct_pose_from_matching_domains( orig_pose,            # pose
                                                               residue1,             # int
                                                               residue2,             # int
-                                                              pdb_file_name,         # '1aaaA.pdb'
-                                                              first_res,            # int
-                                                              second_res   )        # int
+                                                              cur_pose,             # pose
+                                                              cur_first_res,        # int
+                                                              cur_second_res   )    # int
 
             i = i + 1
 
@@ -136,12 +142,9 @@ def find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, log
 def construct_pose_from_matching_domains( host_pose,    # pose
                                           host_res1,    # int
                                           host_res2,    # int
-                                          guest_name,   # '1aaaA.pdb'
+                                          guest_pose,   # pose
                                           guest_res1,   # int
                                           guest_res2 ): # int
-    # build guest pose
-    guest_pose = Pose()
-    pose_from_pdb( guest_pose, 'alpha-beta-hydrolases/'+guest_name )
 
     print host_pose
     print guest_pose
@@ -164,7 +167,6 @@ def construct_pose_from_matching_domains( host_pose,    # pose
     print guest_pose
     pymover.apply(host_pose)
     pymover.apply(guest_pose)
-    raw_input('see pymol for match')
 
     # generate new pose from aligned domains
     new_pose = Pose()
@@ -174,7 +176,10 @@ def construct_pose_from_matching_domains( host_pose,    # pose
     loggy.flush()
 
 
-
+    raw_input('see pymol for match')
+    
+    # continue on to next match
+    pymol.cmd.delete('all')
 
 
 
@@ -184,7 +189,8 @@ if __name__ == '__main__':
 
         try:
             family_dir = sys.argv[-4]
-            pdb_file = sys.argv[-3]
+            orig_pdb_file = sys.argv[-3]                # ./path/to/aaaa.pdb
+            orig_pdb_id = orig_pdb_file.split('/')[-1]  # aaaa.pdb
             residue1 = int(sys.argv[-2])
             residue2 = int(sys.argv[-1])
         except:
@@ -199,9 +205,9 @@ if __name__ == '__main__':
         output_string = '\n\n#######################################\n'+ \
             'This file contains debugging output for thorp.py\n'+ \
             'invoked at: '+str(time.strftime("%Y/%m/%d %H:%M:%S"))+'\n\n\n'+ \
-            'Reference PDB: '+str(pdb_file)+'\n'+ \
+            'Reference PDB: '+str(orig_pdb_file)+'\n'+ \
             'Residues '+str(residue1)+' and '+str(residue2)+'\n'+ \
-            str(len(os.listdir(family_dir)))+' PDBs to posify and traverse for transforms\n\n'
+            str(len(os.listdir(family_dir)))+' dir items (PDBs to posify and traverse for transforms)\n\n'
         loggy.write(output_string)
         loggy.flush()
 
@@ -214,7 +220,7 @@ if __name__ == '__main__':
         matchy.write(matchy_string)
         matchy.flush()
         
-        find_matching_domains( family_dir, pdb_file, residue1, residue2, matchy, loggy )
+        find_matching_domains( family_dir, orig_pdb_file, residue1, residue2, matchy, loggy )
 
         #view_matching_domains()
         ## cycles through by default
