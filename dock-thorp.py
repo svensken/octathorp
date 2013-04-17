7bd5dda9e52a83b0934424a58bcbf612e37b95ed
Kenny <svensken@nbcs.rutgers.edu>
2013-04-17 12:00:48 -0400

    first solution to copying two domains to one pose (two chains)

--- dock-thorp.py
+++ dock-thorp.py
@@ -2,6 +2,7 @@
 
 print 'importing rosetta...'
 from rosetta import *
+import time
 
 
 # silence rosetta's output
@@ -12,18 +13,51 @@ core.init( args )
 
 
 # parameters
-host_pose = Pose()
-pose_from_pdb(host_pose, '')
-guest_pose = Pose()
-pose_from_pdb(guest_pose, '')
-hres1 = 
-hres2 = 
-gres1 = 
-gres2 = 
+hpose = Pose()
+pose_from_pdb(hpose, '4fwb.pdb')
+gpose = Pose()
+pose_from_pdb(gpose, '2yas.pdb')
+hres1 = 22
+hres2 = 33
+gres1 = 44
+gres2 = 77
+
+pi = hpose.pdb_info()
+print pi
+
+
+pymover = PyMOL_Mover()
+#pymover.apply(hpose)
+#raw_input('...')
 
 
 # CHAINS
 #delete hres1-hres2
 #append_residue_by_jump(hpose, gpose.res, hres1, start_new_chain=True)
 #append res of hpose reses
+
+for r in reversed(range(hres1, hres2+1)): # WILL SEGFAULT AT r = 1
+    hpose.delete_polymer_residue( r )
+
+hpose.append_residue_by_jump(gpose.residue(gres1), hres1, start_new_chain=True)
+
+for r in reversed(range(gres1+1, gres2+1)):
+    hpose.append_polymer_residue_after_seqpos(gpose.residue(r), hpose.total_residue(), 0)
+    pi.chain( hpose.total_residue(), "B" )
+    pymover.apply(hpose)
+    time.sleep(.1)
+
+print pi
+
+
+# EXPORT, IMPORT
+hpose.dump_pdb('temp.pdb')
+pose=Pose()
+pose_from_pdb(pose, 'temp.pdb')
+
+print pose.pdb_info()
+
+pymover.apply(pose)
+
+
 
