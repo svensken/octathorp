007cfef3131f9e4aa34612603565b8ef78df9607
svensken <svensken@nbcs.rutgers.edu>
2013-05-07 14:02:59 -0400

    started json regeneration script

---
+++ status-update.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import json
+
+
+#TODO get nstruct and ncpu *programmatically*
+#  ncpu can be read from number of dirs in bashy/
+#  nstruct can be read from source code
+#  these are lame. i want integrated
+
+
+# get something
+
+# get something else
+
+
+for directory in bashy:
+    json_data.append( {
+                        "title"     : str(dirname),
+                        "subtitle"  : "something",
+                        "ranges"    : hmm,
+                        "measures"  : hmm,
+                        "markers"   : hmm
+                      } )
+
+
+with open("/home/svensken/octathorp/d3tst/bullets.json", 'w') as json_file:
+    json. dump( json_data, json_file )
+
+
+print 'klar :D'
