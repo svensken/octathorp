f9d7c38f7206f51bdcec5725581e1d8e2bd0d851
Kenneth Kostenbader <svensken@nbcs.rutgers.edu>
2013-05-10 23:41:53 -0400

    status update working

--- status-update.py
+++ status-update.py
@@ -19,17 +19,22 @@ def take_snapshot():
     
     json_data = []
 
-    for root, dirs, files in os.walk("bashy-old"):
+    #TODO find way to extract nstruct
+    nstruct = 400
+
+    full_total = 0
+
+    for root, dirs, files in os.walk('bashy'):
         
         dirname = os.path.basename(root) # '12'
         # skip unintended dirs
         if not dirname.isdigit():
             continue
-        print dirname
+        #print 'd',dirname
 
         # number of decoys done
-        ndone = len( [f for f in files if f.startswith('output') and f.endswith('.pdb')] )
-        print ndone
+        ndone = len( [f for f in files if f.startswith('manual') and f.endswith('.pdb')] )
+        #print 'n',ndone
 
         # fully worthless decoys
         losers = 0
@@ -44,37 +49,48 @@ def take_snapshot():
         with open( os.path.join(root, 'status.update'), 'r') as timely:
             times = timely.readlines()
             delta_list = []
-            for l in range(times):
+            for l in range(len(times)):
                 try:
-                    t1 = datetime.strptime( times[l], "%Y/%m/%d-%H:%M:%S" )
-                    t2 = datetime.strptime( times[l+1], "%Y/%m/%d-%H:%M:%S" )
+                    t1 = datetime.strptime( times[l][-15:-1], "%Y%m%d-%H%M%S" )
+                    t2 = datetime.strptime( times[l+1][-15:-1], "%Y%m%d-%H%M%S" )
                     delta = t2 - t1
                     delta_list.append( delta.seconds )
                 except:
                     pass
             avg_delta = sum(delta_list) / len(delta_list)
             # could potentially be more statusupdates than pdbs (append mode in dock-thorp.py)
-            npdbs_1hr = 3600 / avg_delta
-            npdbs_10hr = 36000 / avg_delta
-        print avg_delta
+            n1hr = 3600 / avg_delta
+            n10hr = 36000 / avg_delta
+        #print 'a',avg_delta
 
+        nneg = 20 # how to get number of negative energy pdbs?
 
         # add a bar
         json_data.append( {
-                            "title"     : str(dirname),
+                            "title"     : dirname,
                             "subtitle"  : "decoys",
-                            "ranges"    : "50",
-                            "measures"  : "70",
-                            "markers"   : "90"
+                            "ranges"    : [n1hr,n10hr],
+                            "measures"  : [nneg,ndone],
+                            "markers"   : [nstruct]
                           } )
 
+        # i'll plug this in somewhere
+        full_total += ndone
 
-        with open("/home/svensken/octathorp/d3tst/otherbullets.json", 'w') as json_file:
-            print 'a'#json.dump( json_data, json_file )
+
+    with open("/home/svensken/octathorp/d3tst/otherbullets.json", 'w') as json_file:
+        json.dump( json_data, json_file )
 
 
-        print 'klar :D'
+    print 'klar :D'
+    print 'full total:', full_total
+    time_elapsed = datetime.now() - initial_timestamp
+    print 'time elapsed:', time_elapsed
 
+    # things to put in status page header thing
+    # (bulletchart legend)
+    # full_total
+    # initial_timestamp and time_elapsed
 
 
 
