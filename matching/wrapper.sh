c864b5c18c9b9f5a8390c9a5f4df6fc6fc78e74e
Kenneth Kostenbader <svensken@rutgers.edu>
2013-06-15 00:08:41 -0400

    setup csts and pos files for matching

--- matching/wrapper.sh
+++ matching/wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-ncpu=10
+ncpu=30
 
 matchexe="/home/svensken/Rosetta/main/source/bin/match.linuxgccrelease"
 statusupdate="/home/svensken/octathorp/status.update"
 
 
 combopath="/home/svensken/octathorp/matching/combos"
-for combodir in $combopath/3ANS*/ #$combopath/*/
+for combodir in $combopath/2WUF*/ $combopath/3ANS*/ $combopath/3B12*/ $combopath/3GZJ*/ #$combopath/*/
 do
     cd $combodir
     id=${combodir: -10:4}
@@ -15,7 +15,7 @@ do
     flags="@$flagdir/general_match.flags @$flagdir/scaf_gfp.flags @$flagdir/substrate_gfp.flags"
 
     t1=$(date +%s)
-    ls $combodir/*.pdb | xargs -n 1 -P $ncpu    $matchexe $flags -s #arg
+    ls $combodir/*.new.pdb | xargs -n 1 -P $ncpu    $matchexe $flags -s #arg
     t2=$(date +%s)
     echo "$combodir $(expr $t2 - $t1)" >> $statusupdate
 done
---
+++ posify.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+from rosetta import *
+opts = [ 'app', '-database', os.path.abspath( os.environ['PYROSETTA_DATABASE'] ), '-mute', 'all', '-ignore_unrecognized_res']#, '-extra_res_cen', '/home/svensken/octathorp/molfiles/1WM1_PTB.cen.params', '-extra_res_fa', '/home/svensken/octathorp/matching/params/EVS.fa.params', '/home/svensken/octathorp/matching/params/S38.fa.params', '/home/svensken/octathorp/matching/params/FAH.fa.params', '/home/svensken/octathorp/matching/params/old.KEM.fa.params', '/home/svensken/octathorp/matching/params/KEM.fa.params', '/home/svensken/octathorp/matching/params/PTB.fa.params' ]
+args = utility.vector1_string()
+args.extend( opts )
+core.init( args )
+
+#wm = Pose()
+#pose_from_pdb( wm, 'new.1WM1.pdb' )
+
+wu = Pose()
+pose_from_pdb( wu, 'new.2WUF.pdb' )
+
+an = Pose()
+pose_from_pdb( an, 'new.3ANS.pdb' )
+
+b1 = Pose()
+pose_from_pdb( b1, 'new.3B12.pdb' )
+
+gz = Pose()
+pose_from_pdb( gz, 'new.3GZJ.pdb' )
+
+
+
+#pi = wm.pdb_info()
+#print 'wm'
+#for i in [1,2,3,4]:
+#    print i, pi.pdb2pose('A', i)
+
+pi = wu.pdb_info()
+print 'wu'
+for i in [192, 54, 270, 113]: # 269 too far away
+    print i, pi.pdb2pose('A', i)
+
+pi = an.pdb_info()
+print 'an', an.total_residue()
+for i in [335, 524, 496, 383, 466]:
+    print i, pi.pdb2pose('A', i)
+
+pi = b1.pdb_info()
+print 'b1'
+for i in [104, 271, 128, 105, 108, 150, 149, 212]:
+    print i, pi.pdb2pose('A', i)
+
+pi = gz.pdb_info()
+print 'gz'
+for i in [87, 244, 216, 128, 125]:
+    print i, pi.pdb2pose('A', i)
+
+
+
+
+
+
+
+
+
+
+
