a791c01e773b6ffcc334853cfd7cfb4979c858a9
Kenneth Kostenbader <svensken@rutgers.edu>
2013-08-05 15:33:03 -0400

    some more remodeling

--- remodeling/toot/remodeled/wrapper.sh
+++ remodeling/toot/remodeled/wrapper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in {6..10}
+for i in {1..15}
 do
     sleep .5 # time to crtl-c out
     (
-      name=3ANS_3GZJ_$i
+      name=3ANS_native_$i
       mkdir -p $name
       cd $name
       #echo $i | xargs -n 1 -P 10 ../remodel_simple.sh
